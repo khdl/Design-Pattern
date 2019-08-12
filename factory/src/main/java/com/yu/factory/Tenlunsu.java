865228e20504c767314253e523786eb7a2795669
khdl <mykhdl123@gmail.com>
2019-08-12 22:49:43 +0800

    工厂模式

---
+++ factory/src/main/java/com/yu/factory/Tenlunsu.java
@@ -0,0 +1,15 @@
+package com.yu.factory;
+
+/**
+ * @className: Tenlunsu
+ * @author: yu.liu
+ * @date: 2019/8/12 21:34
+ * @description:
+ */
+public class Tenlunsu implements Milk {
+
+    @Override
+    public String getName() {
+        return "特仑苏";
+    }
+}
---
+++ factory/src/main/java/com/yu/factory/Yili.java
@@ -0,0 +1,15 @@
+package com.yu.factory;
+
+/**
+ * @className: Yili
+ * @author: yu.liu
+ * @date: 2019/8/12 21:45
+ * @description:
+ */
+public class Yili implements Milk {
+
+    @Override
+    public String getName() {
+        return "伊利";
+    }
+}
